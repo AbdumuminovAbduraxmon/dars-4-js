3b4eb844467a177365436c3a776eeb7f4232ca38
AbdumuminovAbduraxmon <abduraxmonabdumuminov77@gmail.com>
2022-12-14 11:18:49 +0300

    inital commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var a = 200
+var b = 199
+if(a==b) {
+    var elTitle = document.querySelector('.title')
+    elTitle.textContent = 'teng';
+    console.log(elTitle.textContent);
+}else {
+    var elTitle = document.querySelector('.title')
+    elTitle.textContent = 'teng emas';
+    console.log(elTitle.textContent);
+}
